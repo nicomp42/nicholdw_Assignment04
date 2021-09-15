fe1ecd2571844719f82982d3019220b67a64fd2d
nicomp42 <nicholdw@ucmail.uc.edu>
2021-09-15 10:06:11 -0400

    Add project files.

---
+++ EuclidGCDAlgo/EuclidGCD.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EuclidGCDAlgo
+{
+    public class EuclidGCD
+    {
+        public static long ComputeGCD(long num1, long num2)
+        {
+
+        }
+    }
+}
---
+++ nicholdw_Assignment04/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("nicholdw_Assignment04")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("nicholdw_Assignment04")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("14f885ca-3fc5-49c2-8f59-5063bbf42530")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
