4c5e68047ad11b8e212ccd00fc9efd4274dc5534
nicomp42 <nicholdw@ucmail.uc.edu>
2021-09-15 10:40:12 -0400

    Added test cases

--- EuclidGCDAlgo/EuclidGCD.cs
+++ EuclidGCDAlgo/EuclidGCD.cs
@@ -1,16 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace EuclidGCDAlgo
+﻿
+namespace EuclidGCDNamespace
 {
-    public class EuclidGCD
+/// <summary>
+/// Euclid GCD Computations
+/// </summary>
+public class EuclidGCD
     {
-        public static long ComputeGCD(long num1, long num2)
+        /// <summary>
+        /// Compute the Greatest Common Divisor of two numbers
+        /// Algo at https://simple.wikipedia.org/wiki/Euclidean_algorithm
+        /// </summary>
+        /// <param name="m">First Number</param>
+        /// <param name="n">Second Number</param>
+        /// <returns>GCD of num1 and num2</returns>
+        public static long ComputeGCD(long m, long n)
         {
-
+            if (m < n) { long tmp = m; m = n; n = tmp; } // If num1 < num2, swap the numbers
+            while (n != 0) {
+                long r;
+                r = m % n;
+                m = n;
+                n = r;
+            }
+            return m;
         }
     }
 }
---
+++ nicholdw_Assignment04/EuclidGCDUnitTest.cs
@@ -0,0 +1,77 @@
+﻿using EuclidGCDNamespace;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace EuclidGCDUnitTestNamespace
+{
+    [TestClass]
+    public class EuclidGCDUnitTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(10, EuclidGCD.ComputeGCD(110, 210));
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.AreEqual(3, EuclidGCD.ComputeGCD(300,33));
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Assert.AreEqual(300, EuclidGCD.ComputeGCD(300, 300));
+        }
+        [TestMethod]
+        public void TestMethod4()
+        {
+            Assert.AreEqual(300, EuclidGCD.ComputeGCD(300, 300));
+        }
+        [TestMethod]
+        public void TestMethod5()
+        {
+            Assert.AreEqual(33, EuclidGCD.ComputeGCD(33, 3300000000000000));
+        }
+        [TestMethod]
+        public void TestMethod6()
+        {
+            Assert.AreEqual(1, EuclidGCD.ComputeGCD(9,7));
+        }
+        [TestMethod]
+        public void TestMethod7()
+        {
+            Assert.AreEqual(11, EuclidGCD.ComputeGCD(99, 77));
+        }
+        [TestMethod]
+        public void TestMethod8()
+        {
+            // A big prime and a number just 1 smaller
+            Assert.AreEqual(1, EuclidGCD.ComputeGCD(99999999977, 99999999976));
+        }
+        [TestMethod]
+        public void TestMethod9()
+        {
+            // A big prime and a number just 1 smaller, args swapped from previous test
+            Assert.AreEqual(1, EuclidGCD.ComputeGCD(99999999976, 99999999977));
+        }
+        [TestMethod]
+        public void TestMethod10()
+        {
+            // Two big primes
+            Assert.AreEqual(1, EuclidGCD.ComputeGCD(100123456789, 99999999977));
+        }
+        [TestMethod]
+        public void TestMethod11()
+        {
+            // Two big composites. See https://www.wolframalpha.com/input/?i=gcd+100123456786%2C+99999999978
+            Assert.AreEqual(2, EuclidGCD.ComputeGCD(100123456786, 99999999978));
+        }
+        [TestMethod]
+        public void TestMethod12()
+        {
+            // Two big composites. See https://www.wolframalpha.com/input/?i=gcd+100123456786%2C+99999999978
+            Assert.AreEqual(2, EuclidGCD.ComputeGCD(100123456786, 99999999978));
+        }
+
+    }
+}
