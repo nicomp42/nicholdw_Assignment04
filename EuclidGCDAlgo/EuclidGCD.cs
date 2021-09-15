1b529e44615d07c1943d49fa3cd9e2c81fd94159
nicomp42 <nicholdw@ucmail.uc.edu>
2021-09-15 10:45:27 -0400

    Took out ComputeGCD logic and made it a stub

--- EuclidGCDAlgo/EuclidGCD.cs
+++ EuclidGCDAlgo/EuclidGCD.cs
@@ -15,14 +15,7 @@ public class EuclidGCD
         /// <returns>GCD of num1 and num2</returns>
         public static long ComputeGCD(long m, long n)
         {
-            if (m < n) { long tmp = m; m = n; n = tmp; } // If num1 < num2, swap the numbers
-            while (n != 0) {
-                long r;
-                r = m % n;
-                m = n;
-                n = r;
-            }
-            return m;
+            throw new System.NotImplementedException();
         }
     }
 }
