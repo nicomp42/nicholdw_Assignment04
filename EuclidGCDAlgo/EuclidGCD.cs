c6f9dfd40844043236319756ed4a31a9d3654178
loganfarwick <lflttd21@gmail.com>
2021-09-16 15:59:53 -0400

    Added Euclid's Algorithm to EuclidGCD() method.

--- EuclidGCDAlgo/EuclidGCD.cs
+++ EuclidGCDAlgo/EuclidGCD.cs
@@ -1,4 +1,13 @@
-﻿
+﻿/*
+ * Logan Farwick
+ * farwicls@mail.uc.edu
+ * Course/Term: IT3045C Fall 2022 
+ * Assignment:  04
+ * Due Date: 09/23/2021
+ * Description: Finishing the Euclid Algorithm to make the test cases work
+ * Citations: https://simple.wikipedia.org/wiki/Euclidean_algorithm (psuedo code link from slides)
+ * Anything else: N/A
+ */
 namespace EuclidGCDNamespace
 {
 /// <summary>
@@ -15,7 +24,19 @@ public class EuclidGCD
         /// <returns>GCD of num1 and num2</returns>
         public static long ComputeGCD(long m, long n)
         {
-            throw new System.NotImplementedException();
+            long swap = 0;
+            if (m < n)
+            {
+                swap = m;
+                m = n;
+                n = swap;
+            }
+            while (n != 0)
+            {
+                swap = m % n;
+                m = n;
+                n = swap;
+            } return m;
         }
     }
 }
