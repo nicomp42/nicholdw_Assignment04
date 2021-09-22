0645ddb4bf3a82714719e29208fdbe045e2b8481
LittleJC3 <admiraldolphin31@gmail.com>
2021-09-22 16:58:35 -0400

    ciullajn Created a method that passes all the tests for Euclid's GCD

--- EuclidGCDAlgo/EuclidGCD.cs
+++ EuclidGCDAlgo/EuclidGCD.cs
@@ -15,7 +15,28 @@ public class EuclidGCD
         /// <returns>GCD of num1 and num2</returns>
         public static long ComputeGCD(long m, long n)
         {
-            throw new System.NotImplementedException();
+            // I need to have the larger number on the left so I can divide. I think
+           long placeHolder = 0;
+           if (m < n)
+           // example m = 110 n = 210
+            {
+                placeHolder = m; // placeHolder = 110
+                m = n; // m now is = 210
+                n = placeHolder; // n now is = 110 
+            }
+           while (n != 0) 
+            {
+                placeHolder = m % n; // 210 mod 110 leaves a remainder of 40
+                System.Console.WriteLine(placeHolder);
+                m = n; // Make m equal the lower number, in this case 110
+                System.Console.WriteLine(m);
+                n = placeHolder; // make n what the remainder is so that we can continue to mod until their is no remainder
+                System.Console.WriteLine(n);
+            }
+            System.Console.WriteLine("Final answer = " + m);
+            return m;
+            
+
         }
     }
 }
--- nicholdw_Assignment04/EuclidGCDUnitTest.cs
+++ nicholdw_Assignment04/EuclidGCDUnitTest.cs
@@ -1,4 +1,5 @@
-﻿using EuclidGCDNamespace;
+﻿
+using EuclidGCDNamespace;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 
