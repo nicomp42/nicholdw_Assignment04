b0d5d90fc2d6f68afa39a4f1034ff3a5509b58eb
dominoPC <iamjoeken@gmail.com>
2021-09-23 12:51:10 -0400

    Completed ComputeGCD method of EuclidGCD class.

--- EuclidGCDAlgo/EuclidGCD.cs
+++ EuclidGCDAlgo/EuclidGCD.cs
@@ -1,4 +1,10 @@
-﻿
+﻿/* Joseph Tankersley tankerjh@mail.uc.edu
+ * Contemporary Programming Fall 2021
+ * Assignment 04
+ * 23 September 2021
+ * Class containing a method for finding the GCD of two numbers
+ * Implementation of the Euclidean Algorithm as described by https://simple.wikipedia.org/wiki/Euclidean_algorithm
+ */
 namespace EuclidGCDNamespace
 {
 /// <summary>
@@ -15,7 +21,22 @@ public class EuclidGCD
         /// <returns>GCD of num1 and num2</returns>
         public static long ComputeGCD(long m, long n)
         {
-            throw new System.NotImplementedException();
+            if (m < n) //ensures m is not the smaller of the two numbers
+            {
+                long buffer = m;
+                m = n;
+                n = buffer;
+            }
+
+            long r;
+            while (n != 0)
+            {
+                r = m % n;
+                m = n;
+                n = r;
+            }
+
+            return m;
         }
     }
 }
